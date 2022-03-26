e8293d91097fa73f2f6e76fc6ff0a8a226ba93fb
Ryz1n <102387724+Ryz1n@users.noreply.github.com>
2022-03-26 10:50:04 +0300

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,7 @@
+import requests, re, threading 
+from os import system
+print("dMMMMb  dMP dMP dMMMMMP dMMMMb 
+   dMP.dMP dMP.dMP   .dMP" dMP dMP 
+  dMMMMK"  VMMMMP  .dMP"  dMP dMP  
+ dMP"AMF dA .dMP .dMP"   dMP dMP   
+dMP dMP  VMMMP" dMMMMMP dMP dMP ")
